19f5c880e29d85e9bd205732810586b67185a873
AlexBud96 <AlexSy4ov@gmail.com>
2017-01-10 19:13:58 +0300

    Задача №8

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,43 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5816289
 
 
 def get_user_login():
-    pass
+    login = input('Введите ваш логин VK: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass.getpass('Введите пароль в ВК: ')
+    return password
 
 
 def get_online_friends(login, password):
+
     session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
+         app_id=APP_ID,
+         user_login=login,
+         user_password=password,
+         scope='friends'
+     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends = api.friends.getOnline()
+    if not friends:
+        return []
+    else:
+        users_information = api.users.get(user_ids=friends)
+        return users_information
 
 
 def output_friends_to_console(friends_online):
-    pass
+    if friends_online:
+        print('друзья онлайн : ')
+        for friend in friends_online:
+            print (friend['first_name'],friend['last_name'])
+    else:
+        print('Никто из ваших друзей не онлайн')
 
 if __name__ == '__main__':
     login = get_user_login()
